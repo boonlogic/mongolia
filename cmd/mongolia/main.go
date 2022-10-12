cb16c871e95373a9978480b60cabc3559ee2e20a
Luke Arend <luke@boonlogic.com>
2022-10-12 14:47:59 -0500

    example route: SayHello

--- cmd/mongolia/main.go
+++ cmd/mongolia/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gitlab.boonlogic.com/development/expert/mongolia/pkg/controllers"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/perms", getPerms)
-
+	router.GET("/hello", controllers.SayHello)
 	router.Run("localhost:8080")
 }
