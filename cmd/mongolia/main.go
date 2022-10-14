3ba330c8f696375ac1a1d395106d8519d7f4021a
Luke Arend <luke@boonlogic.com>
2022-10-14 15:48:22 -0500

    clean up main

--- cmd/mongolia/main.go
+++ cmd/mongolia/main.go
@@ -11,11 +11,9 @@ func main() {
 	if err := odm.Configure(); err != nil {
 		log.Fatalf("failed to configure: %s\n", err)
 	}
-
 	if err := odm.Connect(); err != nil {
 		log.Fatalf("failed to connect: %s\n", err)
 	}
-
 	odm.Drop()
 
 	path := "schemas/role.json"
@@ -53,12 +51,6 @@ func main() {
 	}
 	fmt.Printf("created document:\n%+v\n", doc)
 
-	docs, err := roles.FindMany(nil)
-	if err != nil {
-		log.Fatalf("failed to FindMany: %s\n", err)
-	}
-	fmt.Printf("found documents:\n%+v\n", docs)
-
 	doc, err = roles.FindOne(nil)
 	if err != nil {
 		log.Fatalf("failed to FindOne: %s\n", err)
@@ -73,12 +65,17 @@ func main() {
 	obj3.ID = "6349a84fe97051c7b555e175"
 	//obj3.Name = "12347890!@#$!#@$"
 	objs := []any{obj1, obj2, obj3}
-
-	docs, err = roles.CreateMany(objs)
+	docs, err := roles.CreateMany(objs)
 	if err != nil {
 		log.Fatalf("failed to CreateMany: %s\n", err)
 	}
 	fmt.Printf("created documents:\n%+v\n", docs)
+
+	docs, err = roles.FindMany(nil)
+	if err != nil {
+		log.Fatalf("failed to FindMany: %s\n", err)
+	}
+	fmt.Printf("found documents:\n%+v\n", docs)
 
 	//router := gin.Default()
 	//router.GET("/hello", controllers.SayHello)
